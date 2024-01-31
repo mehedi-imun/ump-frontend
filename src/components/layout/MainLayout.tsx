f365afb1157e44a7d65f6d5e1f4e57dd76ae0b68
mahedi <mahedianik4@gmail.com>
2024-01-31 17:03:14 +0600

    refector mainlayout

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,35 +1,8 @@
-import React from "react";
-
-import {
-  AppstoreOutlined,
-  BarChartOutlined,
-  CloudOutlined,
-  ShopOutlined,
-  TeamOutlined,
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-} from "@ant-design/icons";
-import { Layout, MenuProps } from "antd";
+import { Layout } from "antd";
 import { Button } from "antd/es/radio";
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
 const { Header, Content } = Layout;
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const items: MenuProps["items"] = [
-  UserOutlined,
-  VideoCameraOutlined,
-  UploadOutlined,
-  BarChartOutlined,
-  CloudOutlined,
-  AppstoreOutlined,
-  TeamOutlined,
-  ShopOutlined,
-].map((icon, index) => ({
-  key: String(index + 1),
-  icon: React.createElement(icon),
-  label: `nav ${index + 1}`,
-}));
 const MainLayout = () => {
   return (
     <Layout style={{ height: "100vh" }}>

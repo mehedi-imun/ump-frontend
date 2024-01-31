4a361c60ddec3a226dfa2455ec83c93558e44bba
mahedi <mahedianik4@gmail.com>
2024-01-31 17:00:53 +0600

    sync with genrate sidebar utils with sidebar component

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,67 +1,56 @@
+import React from "react";
 
-import React from 'react';
-
-import { Layout, Menu, MenuProps } from 'antd';
 import {
-    AppstoreOutlined,
-    BarChartOutlined,
-    CloudOutlined,
-    ShopOutlined,
-    TeamOutlined,
-    UploadOutlined,
-    UserOutlined,
-    VideoCameraOutlined,
-  } from '@ant-design/icons';
-import { Outlet } from 'react-router-dom';
-const { Header, Content, Footer, Sider } = Layout;
-const items: MenuProps['items'] = [
-    UserOutlined,
-    VideoCameraOutlined,
-    UploadOutlined,
-    BarChartOutlined,
-    CloudOutlined,
-    AppstoreOutlined,
-    TeamOutlined,
-    ShopOutlined,
-  ].map((icon, index) => ({
-    key: String(index + 1),
-    icon: React.createElement(icon),
-    label: `nav ${index + 1}`,
-  }));
+  AppstoreOutlined,
+  BarChartOutlined,
+  CloudOutlined,
+  ShopOutlined,
+  TeamOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons";
+import { Layout, MenuProps } from "antd";
+import { Button } from "antd/es/radio";
+import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
+const { Header, Content } = Layout;
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const items: MenuProps["items"] = [
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+  BarChartOutlined,
+  CloudOutlined,
+  AppstoreOutlined,
+  TeamOutlined,
+  ShopOutlined,
+].map((icon, index) => ({
+  key: String(index + 1),
+  icon: React.createElement(icon),
+  label: `nav ${index + 1}`,
+}));
 const MainLayout = () => {
-    return (
-        <Layout style={{height:"100vh"}}>
-        <Sider
-          breakpoint="lg"
-          collapsedWidth="0"
-          onBreakpoint={(broken) => {
-            console.log(broken);
-          }}
-          onCollapse={(collapsed, type) => {
-            console.log(collapsed, type);
-          }}
-        >
-          <div className="demo-logo-vertical" />
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items} />
-        </Sider>
-        <Layout>
-          <Header style={{ padding: 0}} />
-          <Content style={{ margin: '24px 16px 0' }}>
-            <div
-              style={{
-                padding: 24,
-                minHeight: 360,
-              }}
-            >
-            <Outlet></Outlet>
-            </div>
-          </Content>
-          <Footer style={{ textAlign: 'center' }}>
-            Ant Design ©{new Date().getFullYear()} Created by Ant UED
-          </Footer>
-        </Layout>
+  return (
+    <Layout style={{ height: "100vh" }}>
+      <Sidebar />
+      <Layout>
+        <Header>
+          <Button>Logout</Button>{" "}
+        </Header>
+        <Content style={{ margin: "24px 16px 0" }}>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+            }}
+          >
+            <Outlet />
+          </div>
+        </Content>
       </Layout>
-    );
+    </Layout>
+  );
 };
 
 export default MainLayout;

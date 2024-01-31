299644de19c4910635092b0ce5fae859f2a40ff6
mahedi <mahedianik4@gmail.com>
2024-01-31 16:47:47 +0600

    add sider bar genarate utils function

---
+++ src/utils/sidebarItemsGenerator.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { TSidebarItem, TUserPath } from '../types/sidebar.type';
+import { NavLink } from 'react-router-dom';
+
+export const sidebarItemsGenerator = (items: TUserPath[], role: any) => {
+  const sidebarItems = items.reduce((acc: TSidebarItem[], item) => {
+    if (item.path && item.name) {
+      acc.push({
+        key: item.name,
+        label: <NavLink to={`/${role}/${item.path}`}>{item.name}</NavLink>,
+      });
+    }
+
+    if (item.children) {
+      acc.push({
+        key: item.name,
+        label: item.name,
+        children: item.children.map((child) => ({
+          key: child.name,
+          label: <NavLink to={`/${role}/${child.path}`}>{child.name}</NavLink>,
+        })),
+      });
+    }
+
+    return acc;
+  }, []);
+
+  return sidebarItems;
+};

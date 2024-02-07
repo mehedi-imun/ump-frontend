23a5165796a29a885f6f269690e8296bd70f13bd
mahedi <mehediimun@gmail.com>
2024-02-07 19:09:02 +0600

    add  semester table for show academic sesmester data

--- src/pages/admin/academicManagement/AcademicSemester.tsx
+++ src/pages/admin/academicManagement/AcademicSemester.tsx
@@ -1,10 +1,75 @@
-import React from "react";
+import type { TableColumnsType, TableProps } from 'antd';
+import { Table } from 'antd';
+import React from 'react';
 import { useGetAllSemestersQuery } from "../../../redux/features/admin/academicManagement/academicSemesterApi";
 
+interface DataType {
+  key: React.Key;
+  name: string;
+  age: number;
+  address: string;
+}
+
 const AcademicSemester = () => {
-  const data = useGetAllSemestersQuery(undefined);
-  console.log(data);
-  return <div>this is academic semester component</div>;
+  const {data:semesterData} = useGetAllSemestersQuery(undefined);
+  const tableData = semesterData?.data.map(({_id,name, startMonth,endMonth,year} )=> ({
+
+    _id,
+    name,
+     startMonth,
+     endMonth,
+     year
+  }) )
+  const columns: TableColumnsType<DataType> = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      filters: [
+        {
+          text: 'Joe',
+          value: 'Joe',
+        },
+        {
+          text: 'Jim',
+          value: 'Jim',
+        },
+        {
+          text: 'Submenu',
+          value: 'Submenu',
+          children: [
+            {
+              text: 'Green',
+              value: 'Green',
+            },
+            {
+              text: 'Black',
+              value: 'Black',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      title: 'Year',
+      dataIndex: 'year',
+      defaultSortOrder: 'descend',
+    },
+    {
+      title: 'Start month',
+      dataIndex: 'startMonth',
+      
+    },
+    {
+      title: 'End month',
+      dataIndex: 'endMonth',
+      
+    },
+  ];
+
+  const onChange: TableProps<DataType>['onChange'] = (pagination, filters, sorter, extra) => {
+    console.log('params', pagination, filters, sorter, extra);
+  };
+  return <div><Table columns={columns} dataSource={tableData} onChange={onChange} /></div>;
 };
 
 export default AcademicSemester;

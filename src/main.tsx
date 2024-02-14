fca2325054a2dc1d6861ff251d1fa9b5ee0d63ec
mehedi-imun <mehediimun@gmail.com>
2024-02-14 16:55:28 +0600

    chenge env

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
-import { PersistGate } from "redux-persist/integration/react";
+import { PersistGate } from 'redux-persist/integration/react'
 import { Toaster } from "sonner";
 import "./index.css";
 import { persister, store } from "./redux/store.ts";
